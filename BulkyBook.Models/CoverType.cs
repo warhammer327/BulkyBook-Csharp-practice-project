02c54e7238bfa5661703b0156be27d7fa9ba7547
warhammer327 <rizwansm@outlook.com>
2022-10-10 19:18:15 +0600

    added cover type to db

---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Display Name")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
