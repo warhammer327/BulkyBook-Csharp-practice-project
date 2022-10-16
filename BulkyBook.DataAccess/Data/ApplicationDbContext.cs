b0ccb3f58470b4a555e3ac83f96cca96af12b893
warhammer327 <rizwansm@outlook.com>
2022-10-16 17:17:24 +0600

    added product model

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace BulkyBookWeb.Controllers.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int Price50 { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int Price100 { get; set; }
+        public string ImageURL { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public Category Category { get; set; }
+        [Required]
+        public int CoverTypeId { get; set; }
+        [Required]
+        public CoverType CoverType { get; set; }
+    }
+}
