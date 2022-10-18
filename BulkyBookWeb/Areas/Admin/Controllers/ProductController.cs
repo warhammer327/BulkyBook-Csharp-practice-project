fb31aacfdacfe0dffb25dcd502f65933dde9f25a
warhammer327 <rizwansm@outlook.com>
2022-10-18 16:13:45 +0600

    implemented viewdata

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -34,7 +34,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             if (id == null || id == 0)
             {
                 ViewBag.CategoryList = CategoryList;
-                ViewBag.CoverTypeList = CoverTypeList;
+                ViewData["CoverTypeList"] = CoverTypeList;
                 return View(prod);
 
             }
